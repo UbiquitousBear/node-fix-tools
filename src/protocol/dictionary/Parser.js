cfeb951d69f5cb6abeba3ca4f605e232c0500cc3
Shamil Nunhuck <shamil@shamil.co.uk>
2018-12-31 12:16:18 +0000

    Added specification structures, message and FIX4.2 definition

---
+++ src/protocol/dictionary/Parser.js
@@ -0,0 +1,53 @@
+import Dictionary from './Dictionary'
+import Tag from './Tag/Tag'
+import TagContainer from './Tag/TagContainer'
+import MessageType from './MessageType/MessageType'
+import MessageTypeContainer from './MessageType/MessageTypeContainer'
+import Structure from './Structure'
+
+export function parseToDictionary ({ data }) {
+	let tags = [],
+		messageTypes = [],
+		sessionMessageTypes = [],
+		structureHeaderTags = [],
+		structureTrailerTags = []
+		
+		data.tags.forEach(
+			element => tags.push(
+				new Tag({ value: element.tag, name: element.name })
+			)
+		)
+
+		data.message_types.forEach(
+			element => messageTypes.push(
+				new MessageType({ msgType: element.msgType, name: element.name }))
+		)
+
+		data.structure.header.forEach(
+			element => structureHeaderTags.push(
+				new Tag({ value: element.tag, name: element.name })
+			)
+		)
+
+		data.structure.trailer.forEach(
+			element => structureTrailerTags.push(
+				new Tag({ value: element.tag, name: element.name })
+			)
+		)
+
+		data.session_message_types.forEach(
+			element => sessionMessageTypes.push(element)
+		)
+
+		return new Dictionary({
+			dialectName: data.dialect_name,
+			versionString: data.fix_version,
+			structure: new Structure({
+				header: new TagContainer({ tags: structureHeaderTags }),
+				trailer: new TagContainer({ tags: structureTrailerTags })
+			}),
+			tags: new TagContainer({ tags }),
+			messageTypes: new MessageTypeContainer({ messageTypes }),
+			sessionMessageTypes
+		})
+}
